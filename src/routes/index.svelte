b9f92a0f35b90182af7cf913d55571169f731e16
LeeKuanGim <leekuangim@gmail.com>
2021-12-19 02:44:51 +0000

    commit 1

---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let location = "";
+    let temperature = "";
+    let description = "";
+</script>
+<h1>Weather App</h1>
+
+<form on:submit|preventDefault={getWeather}>
+    <label for="location">Location:</label>
+    <input id="location" bind:value={location} type="text" />
+    <button type="submit">Check</button>
+</form>
